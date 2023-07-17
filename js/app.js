6c932d7dc15616fc9de4343285d134fd595ae8d5
Cecilia <barboza.ceciliam@gmail.com>
2023-07-17 01:24:33 -0300

    styles(Contact): WIP, add onfocus event

--- js/app.js
+++ js/app.js
@@ -6,4 +6,33 @@ inputs.forEach(input => {
     input.addEventListener('blur', (input)=>{
         validatorInput(input.target)
     })
+    
+});
+
+
+// export function inputLabel(input) {
+//     document.querySelector(".input__field");
+
+//     inputLabel.addEventListener("focus", ()=>{
+//         console.log("click")
+//         document.querySelector(".input__label").style.top = '-20px';
+//         document.querySelector(".input__label").style.top = '-20px';    
+//     });
+// }
+
+
+
+const input = document.querySelector(".input__field");
+const label = document.querySelector(".input__label");
+    input.addEventListener("focus", function(){
+    label.style.display = 'inline';
+    console.log("click")
+});
+
+input.addEventListener("blur", function(){
+    label.style.top= '-20px';
+    label.style.color = '#007bff';
+    label.style.width= "100%";
+    label.style.fontSize= "12px";
+    label.style.fontFamily= "Raleway";
 });
